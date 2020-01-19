8e4739a54207ae4cd47c6cb587ad3a1b8fdeb546
Kyryll Parolys <kyryllpar@gmail.com>
2020-01-19 17:12:52 +0200

    Added swagger.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,8 +17,29 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_yasg import openapi
+from drf_yasg.inspectors import SwaggerAutoSchema
+from drf_yasg.views import get_schema_view
+from rest_framework.permissions import AllowAny
+
+
+class CategorizedAutoSchema(SwaggerAutoSchema):
+    def get_tags(self, operation_keys):
+        if len(operation_keys) >= 1:
+            operation_keys = operation_keys[1:]
+        return super().get_tags(operation_keys)
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Virkoni API",
+        default_version='v1',
+    ),
+    permission_classes=(AllowAny,),
+)
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('searcher/', include('recognizer.urls')),
-              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('admin/', admin.site.urls),
+    path('searcher/', include('recognizer.urls')),
+    path('docs/', schema_view.with_ui(), name='docs'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
