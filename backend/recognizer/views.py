7580a77afdde0f71860d2839febdf8d18976086f
vladkiriyak <vladkiriyak1@gmail.com>
2020-01-21 12:15:12 +0200

    add CORS and refactoring

--- backend/recognizer/views.py
+++ backend/recognizer/views.py
@@ -2,59 +2,35 @@ import requests
 from django.core.files.storage import FileSystemStorage
 from django.http import JsonResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
+from backend.settings import CONFIG
 
 
-
-# @csrf_exempt
-# def get_song(request):
-#     form = UploadFileForm(request.POST, request.FILES)
-#
-#     file_name = get_file_name()
-#     handle_uploaded_file(file_name, request.FILES['file'])
-#
-    # data = {
-    #     'url': f'{config["base_url"]}/static/{file_name}',
-    #     'return': 'deezer',
-    #     'api_token': config["audd.io_token"]
-    # }
-#
-    # result = requests.post('https://api.audd.io/', data=data).json()
-    #
-    # return JsonResponse(
-    #     data={
-    #         "status": result['status'],
-    #         "song_name": result['result']['deezer']['title'],
-    #         "path_to_artist_image": result['result']['deezer']['artist']['picture'],
-    #         "path_to_audio": result['result']['deezer']['preview'],
-    #     }
-    # )
-
-
+@csrf_exempt
 def upload_file(request):
-    counter = 1
-    if request.method == 'POST' and request.FILES['myfile']:
-        myfile = request.FILES['myfile']
+    if request.method == 'POST' and request.FILES['file']:
+        file = request.FILES['file']
         fs = FileSystemStorage()
 
-        filename = fs.save(str(counter)+'.mp3', myfile)
-        counter += 1
+        filename = fs.save('', file)
 
         uploaded_file_url = fs.url(filename)
 
-        # return render(request, 'upload.html', {
-        #     'uploaded_file_url': uploaded_file_url
-        # })
-
         return redirect('get-song', file_url=uploaded_file_url)
 
-    return render(request, 'upload.html')
+    else:
+        return JsonResponse(
+            data={
+                "status": 'Upload error',
+            }
+        )
 
 
 def get_song(request, file_url):
     data = {
-        'url': ''+file_url,  # place tunnel url before file_url
+        'url': CONFIG['base_url'] + file_url,
         'return': 'deezer',
-        'api_token': '80af3a2f63cc041bd1661be79a63c322'
+        'api_token': CONFIG['audd.io_token']
     }
 
     res = requests.post('https://api.audd.io/', data=data).json()
