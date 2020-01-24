02e79e09d8dd9cacb8616f30290c15d6fab865d7
Kyryll Parolys <kyryllpar@gmail.com>
2020-01-24 09:53:19 +0200

    Fixed up urls.

--- backend/recognizer/urls.py
+++ backend/recognizer/urls.py
@@ -5,5 +5,5 @@ from recognizer import views
 urlpatterns = [
     path('upload_file', views.upload_file, name='upload-file'),
     path('get_by_song/<path:file_url>', views.get_by_song, name='get-by-song'),
-    path('get_by_humming/<path:file_url>', views.get_by_humming, name='get-by-humming'),
+    # path('get_by_humming/<path:file_url>', views.get_by_humming, name='get-by-humming'),
 ]
