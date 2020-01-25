de4ca85b9d05f555250f13e73a14b51b53f1ecc3
Mykola Kotsabiuk <kotsabiukmv98@ukr.net>
2020-01-25 23:11:03 +0200

    Updates

--- backend/backend/views.py
+++
@@ -1,9 +0,0 @@
-import time
-
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-
-
-@csrf_exempt
-def main(request):
-    return HttpResponse(content=time.asctime(time.gmtime()))
